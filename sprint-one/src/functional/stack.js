bd17db534b4c46ce6d651ba20880df3bb420a8fc
Amanda Winkles <amandawinkles@gmail.com>
2020-04-05 19:51:06 -0500

    Complete functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
+
+   //push(string) - Add a string to the top of the stack
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
-  someInstance.pop = function() {
+  //pop() - Remove and return the string on the top of the stack
+  someInstance.pop = function(value) {
+    count--;
+    if (count < 0) {
+      count = 0;
+    }
+    return storage[count];
+    delete storage[count];
   };
 
+  //size() - Return the number of items on the stack
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
