632547582f20ec6280db480c67ab0a4cdacb0c31
Amanda Winkles <amandawinkles@gmail.com>
2020-04-07 11:02:06 -0500

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.newQueue = {};
+    this.newQueue.storage = {};
+    this.newQueue.index = 0;
+    this.newQueue.count = 0;
+    this.newQueue.fifo = 0;
   }
 
+  enqueue(value) {
+    this.newQueue.storage[this.newQueue.index] = value;
+    this.newQueue.index++;
+    this.newQueue.count++;
+  }
+
+  dequeue() {
+    if (this.newQueue.count > 0) {
+      this.newQueue.count--;
+    }
+    var key = this.newQueue.storage[this.newQueue.fifo];
+    this.newQueue.fifo++;
+    return key;
+  }
+
+  size() {
+    return this.newQueue.count;
+  }
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.newStack = {};
+    this.newStack.storage = {};
+    this.newStack.count = 0;
   }
 
+  push(value) {
+    this.newStack.storage[this.newStack.count] = value;
+    this.newStack.count++;
+  }
+
+  pop() {
+    if (this.newStack.count > 0) {
+      this.newStack.count--;
+    }
+    return this.newStack.storage[this.newStack.count];
+  }
+
+  size() {
+    return this.newStack.count;
+  }
 }
