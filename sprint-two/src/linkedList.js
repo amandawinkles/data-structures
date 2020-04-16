673e5a41ad4583c9d5c186dae58ff063298d36da
Amanda Winkles <amandawinkles@gmail.com>
2020-04-15 19:08:42 -0500

    pseudocoded linkedList and tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,20 +1,3 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
-  };
-
-  list.removeHead = function() {
-  };
-
-  list.contains = function(target) {
-  };
-
-  return list;
-};
-
 var Node = function(value) {
   var node = {};
 
@@ -22,6 +5,40 @@ var Node = function(value) {
   node.next = null;
 
   return node;
+};
+
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+  //var currentNode;
+
+  list.addToTail = function(value) {
+    //var node = Node(value)
+    //if (list.tail)
+      //list.tail.next = node
+    //else
+      //list.head = node
+      //list.tail = node
+  };
+
+  list.removeHead = function() {
+    //if tail doesn't exist, return null
+    //set current node to list.head
+    //set currentNode.next to list.head, changing pointer/connections to delete head
+    //return list.head.value
+  };
+
+  list.contains = function(target) {
+    //set currentNode to equal list.head
+    //while loop to iterate through list while currentNode exists (isn't null)
+      //if currentNode.value === target
+        //return true
+      //set currentNode to currentNode.next
+    //return false
+  };
+
+  return list;
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,21 +3,33 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  //_.extend(newTree, treeMethods)
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) { //add target as a parameter?
+  //set value to new child node? //var child = Tree(value)
+  //push child node into this.children array
 };
 
 treeMethods.contains = function(target) {
+  //_.contains(Tree, target)?
+
+  //if current node value is equal to the target
+    //return true
+  //for loop to iterate through children
+    //call treeMethods.contains on each child
+  //return false
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    //addChild: O(1)
+    //contains: O(n)
  */
