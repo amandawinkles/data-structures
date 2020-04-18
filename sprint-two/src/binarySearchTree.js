f697435f8b9ad821b01a9da5a19a4cd896231eb6
Amanda Winkles <amandawinkles@gmail.com>
2020-04-18 17:12:43 -0500

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,107 +1,87 @@
-class Node {
-  constructor(value) {
-    this.left = null;
-    this.right = null;
-    this.value = value;
-  }
-}
+//prototypal
+var BinarySearchTree = function(value) {
+  // define tree with prototype instantiation
+  var tree = Object.create(binarySearchTreeMethods);
+  // define tree value
+  tree.value = value;
+  // define left and right property
+  tree.left = null;
+  tree.right = null;
 
-class BinarySearchTree {
-  constructor(value) {
-    this.root = new Node(value);
-    this.count = 1;
-  }
-
-  insert(value) {
-    //define current variable & set to this
-    var current = this;
-    //if this.root exists, set current to equal this.root
-    if (this.root) {
-      current = this.root;
-    }
-    //if current.value is greater than value
-    if (current.value > value) {
-      //if current.left doesn't exist,
-      if (!current.left) {
-        //set current.left to equal new Node instance w/value
-        current.left = new Node(value);
-        //this.count++
-        this.count++;
-        //return?
-      }
-      //if current.left exists, set current.left as this object and call this.insert w/value as second parameter to recurse
-      if (current.left) {
-        this.insert.call(current.left, value);
-      }
-      //return current
-      return current;
-    }
-    //if current.value is less than value
-    if (current.value < value) {
-      //if current.right doesn't exist
-      if (!current.right) {
-        //set current.right to equal a new Node instance w/value passed in
-        current.right = new Node(value);
-        //update count
-        this.count++;
-        //return?
-      }
-      //if current.right exists,
-      if (current.right) {
-        //set current.right as this object and call this.insert w/value as second argument
-        this.insert.call(current.right, value);
-      }
-    }
-  };
-
-  contains(value) {
-    //define current & set to this
-    var current = this;
-    //if this.root exists, set current to root
-    if (this.root) {
-      current = this.root;
-    }
-    //iterate through tree w/while as long as current exists/not undefined
-    while (current) {
-      //if value equals === current.value, check target value existence
-      if (value === current.value) {
-        return true;
-      }
-      //if target value is less than current.value, check left
-      if (value < current.value) {
-        current = current.left;
-      }
-      //if value is more, check right
-      if (value > current.value) {
-        current = current.right;
-      }
-    }
-    //if not found, return false
-    return false;
-  };
-
-  depthFirstLog(cb) {
-    //create inner function & pass in node
-      //check if nodes aren't null, then recurse each node, node.left, node.right, this.root
-    var recurseNodes = function(node) {
-      if (node.left !== null) {
-        recurseNodes(node.left);
-      }
-      if (node.right !== null) {
-        recurseNodes(node.right);
-      }
-      //call callback w/this, then node.value as argument
-      cb.call(this, node.value);
-    }
-    recurseNodes(this.root);
-  }
+  // return tree
+  return tree;
 };
 
-var bst = new BinarySearchTree(5);
-bst.insert(9);
-bst.insert(3);
+// define binary search tree methods
+var binarySearchTreeMethods = {
+
+  // insert method, accepts a value and places it in the tree in the correct position
+  insert(value) {
+    // define current BST child
+    var child = BinarySearchTree(value);
+    // recurse through BST
+    // if current value is less than current tree value
+    if (value < this.value) {
+      // and the left node is empty
+      if (this.left === null) {
+      // left tree node becomes child tree
+        this.left = child;
+      // otherwise
+      } else {
+      // set the left node as the child value
+        this.left.insert(child.value);
+      }
+    // otherwise
+    // if current value is greater than current tree value
+    } else if (value > this.value) {
+      // and right node is empty
+      if (this.right === null) {
+      // right tree node comes child tree
+        this.right = child;
+      // otherwise
+      } else {
+      // set the right node as the child value
+        this.right.insert(child.value);
+      }
+    }
+  },
+
+  // contains method, accepts a value and returns a boolean reflecting whether or not the value is contained in the tree
+  contains(target) {
+  // if current value is equal to target value
+    if (this.value === target) {
+      // return true
+      return true;
+    }
+
+    // if target is smaller than current value
+    if (target < this.value) {
+      // return boolean from checking if left tree contains target AND isn't null
+      return this.left !== null && this.left.contains(target);
+    // otherwise
+    } else {
+      // return boolean from checking if right tree contains target AND isn't null
+      return this.right !== null && this.right.contains(target);
+    }
+  },
+
+  // depth first log method, accepts a callback and executs it on every value contained in tree
+  depthFirstLog(callBack) {
+  // pass current tree value to callback function
+    callBack(this.value);
+    // set children of tree to iterate through
+    var children = [this.left, this.right];
+    // iterate through each child of children, applying callback function
+    _.each(children, function(child) {
+      child && child.depthFirstLog(callBack);
+    });
+  }
+};
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  insert: O (log n)
+  contains: O (log n)
+  depthFirstLog: O (log n)
  */
