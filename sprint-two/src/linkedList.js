290bed2cd7170b9ad4fbe4ed98ed629502737303
Amanda Winkles <amandawinkles@gmail.com>
2020-04-17 21:04:42 -0500

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -11,31 +11,44 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-  //var currentNode;
 
   list.addToTail = function(value) {
     //var node = Node(value)
+    var node = Node(value);
+    list.head = list.head || node;
     //if (list.tail)
+    if (list.tail) {
       //list.tail.next = node
-    //else
-      //list.head = node
-      //list.tail = node
+      list.tail.next = node;
+    }
+    //list.tail = node
+    list.tail = node;
   };
 
   list.removeHead = function() {
-    //if tail doesn't exist, return null
-    //set current node to list.head
-    //set currentNode.next to list.head, changing pointer/connections to delete head
-    //return list.head.value
+    // store value of current head node
+    var head = list.head;
+    // set new head node
+    list.head = list.head.next;
+    // return removed value
+    return head.value;
   };
 
   list.contains = function(target) {
     //set currentNode to equal list.head
+    var current = list.head;
     //while loop to iterate through list while currentNode exists (isn't null)
+    while (current) {
       //if currentNode.value === target
-        //return true
+      if (current.value === target) {
+      //return true
+        return true;
+      }
       //set currentNode to currentNode.next
+      current = current.next;
+    }
     //return false
+    return false;
   };
 
   return list;
