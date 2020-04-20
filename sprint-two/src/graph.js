5c946998280ec881072ffadf04d7111fbc643a86
Amanda Winkles <amandawinkles@gmail.com>
2020-04-20 11:44:38 -0500

    Complete Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -50,7 +50,7 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
   for (var key in this.storage) {
-    cb(this.storage[key]);
+    cb(key);
   }
 };
 // for each item in storage
